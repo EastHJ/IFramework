7b0c97d055702bb744a5e3c4543c0ddf8ac4e24d
EastHJ <hjindwhut@126.com>
2018-09-13 18:55:21 +0800

    init

---
+++ src/main/java/com/cambrain/cambrainframework/CambrainframeworkApplication.java
@@ -0,0 +1,12 @@
+package com.cambrain.cambrainframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CambrainframeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CambrainframeworkApplication.class, args);
+    }
+}
